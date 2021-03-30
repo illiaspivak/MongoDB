11fd59df773af948520521cb8ab938d49e004edb
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-03-30 09:31:05 +0200

    New project MongoDB and Java

---
+++ src/main/java/sk/kosickaakademia/spivak/hobby/App.java
@@ -0,0 +1,13 @@
+package sk.kosickaakademia.spivak.hobby;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
